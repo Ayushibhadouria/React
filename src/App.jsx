c4d1f5b80146bdeb125602312666ef856e21ace0
ayushi <ayushisinghbhadouria@gmail.com>
2025-04-26 10:12:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div style={{border:"1px solid black", padding:"10px", fontSize:"30px"}}>This is first github session</div>
+  )
+}
+
+export default App
